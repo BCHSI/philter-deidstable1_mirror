2b7717df0b0b295720197137538b4f0a064f1009
blakedallen <blakedallen@gmail.com>
2018-02-20 00:28:33 -0800

    now performing Recall: 80.02% Precision: 90.52%, issue is certain regex's will degrade performance if they are too general, next step may be to use context windows to get smarter rules

--- numbers/main.py
+++ numbers/main.py
@@ -7,7 +7,7 @@ def main():
     ap.add_argument("-i", "--input", default="./data/i2b2_notes/",
                     help="Path to the directory or the file that contains the PHI note, the default is ./data/i2b2_notes/",
                     type=str)
-    ap.add_argument("-a", "--anno", default="./data/i2b2_anno/",
+    ap.add_argument("-a", "--anno", default="./data/i2b2_annotations_with_symbols/",
                     help="Path to the directory or the file that contains the PHI note, the default is ./data/i2b2_notes/",
                     type=str)
     ap.add_argument("-o", "--output", default="./data/i2b2_results/",
@@ -26,6 +26,7 @@ def main():
         "foutpath":args.output,
         "anno_folder":args.anno,
         "regex":args.regex,
+        "anno_suffix":"_all_characters_phi_reduced.ano" #_phi_reduced.ano
     }
 
     path = "premade" #can be premade regex or generated
@@ -49,7 +50,38 @@ def main():
         filterer.transform(coord_map_name="genfilter")
 
 
-    filterer.eval(only_digits=True)
+    filterer.eval(only_digits=True, 
+        fp_output="data/phi/phi_fp/phi_fp.json",
+        fn_output="data/phi/phi_fn/phi_fn.json")
+    #now map the phi we're missing
+    #map false negatives and generate regex
+    filterer.mapphi(phi_path="data/phi/phi_fn/phi_fn.json", 
+            out_path="data/phi/phi_fn/phi_fn_map.json",  
+            digit_char="#", 
+            string_char="*")
+    filterer.gen_regex(
+        source_map="data/phi/phi_fn/phi_fn_map.json", 
+        regex_map_name="gen_fn", 
+        output_file="data/phi/phi_fn/phi_fn_regex.json",
+        output_folder="data/phi/phi_fn/patterns/",
+        digit_char="#", 
+        string_char="*"
+    )
+
+    #map false positives
+    filterer.mapphi(phi_path="data/phi/phi_fp/phi_fp.json", 
+            out_path="data/phi/phi_fp/phi_fp_map.json",  
+            digit_char="#", 
+            string_char="*")
+    filterer.gen_regex(
+        source_map="data/phi/phi_fp/phi_fp_map.json", 
+        regex_map_name="gen_fp", 
+        output_file="data/phi/phi_fp/phi_fp_regex.json",
+        output_folder="data/phi/phi_fp/patterns/",
+        digit_char="#", 
+        string_char="*"
+    )
+
 
 
 if __name__ == "__main__":
