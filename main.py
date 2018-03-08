69aa8bd402e967318a2b741e543793a23162bebd
blakedallen <blakedallen@gmail.com>
2018-03-08 02:38:45 -0500

    adding in initial test folder structure

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     ap.add_argument("-o", "--output", default="./data/i2b2_results/",
                     help="Path to the directory to save the PHI-reduced notes in, the default is ./data/i2b2_results/",
                     type=str)
-    ap.add_argument("-f", "--filters", default="./configs/just_digits.json",
+    ap.add_argument("-f", "--filters", default="./configs/test_set.json",
                     help="Path to our config file",
                     type=str)
     ap.add_argument("-d", "--debug", default=True,
@@ -54,21 +54,33 @@ def main():
         fn_output="data/phi/fn.json",
         fp_output="data/phi/fp.json")
 
-    filterer.eval(
-        in_path="data/i2b2_anno/",
-        anno_path="data/i2b2_anno/",
-        anno_suffix=".txt",
-        summary_output="data/phi/summary.json",
-        fn_output="data/phi/fn.json",
-        fp_output="data/phi/fp.json")
 
-    filterer.eval(
-        in_path="data/i2b2_notes/",
-        anno_path="data/i2b2_anno/",
-        anno_suffix=".txt",
-        summary_output="data/phi/summary.json",
-        fn_output="data/phi/fn.json",
-        fp_output="data/phi/fp.json")
+    #test evals
+
+    # filterer.eval(
+    #     in_path="data/i2b2_anno/",
+    #     anno_path="data/i2b2_anno/",
+    #     anno_suffix=".txt",
+    #     summary_output="data/phi/summary.json",
+    #     fn_output="data/phi/fn.json",
+    #     fp_output="data/phi/fp.json")
+
+    # filterer.eval(
+    #     in_path="data/i2b2_notes/",
+    #     anno_path="data/i2b2_anno/",
+    #     anno_suffix=".txt",
+    #     summary_output="data/phi/summary.json",
+    #     fn_output="data/phi/fn.json",
+    #     fp_output="data/phi/fp.json")
+
+    # filterer.eval(
+    #     in_path="data/i2b2_results_test/",
+    #     anno_path="data/i2b2_anno_test/",
+    #     anno_suffix=".txt",
+    #     summary_output="data/phi/summary.json",
+    #     fn_output="data/phi/fn.json",
+    #     fp_output="data/phi/fp.json")
+
 
         
         
