952d5f6e7aab3677b8a4f942a24e040572388de6
Kathleen <kdmuenzen@yahoo.com>
2018-05-21 15:08:37 -0700

    updating eval script versions

---
+++ getFNcategory_fred.py
@@ -0,0 +1,83 @@
+import pickle
+import os
+
+
+def getcatgory(summary, anno_path):
+    '''
+    return FN's category
+    input:
+    summary: dict[filename: dict[false_negative, false_positive, ture_positive]]
+    anno_path: path to the annotation files
+    output:
+    FN_category: list["FN type"]
+    FN_dict: dict[type:[FNs]]
+    '''
+
+    FN_category = []
+    FN_dict = {}
+    FN_dict['cannot find'] = []
+    category_dict = {'0':'safe', '1':'phi', '2':'FP',
+                     'n':'Name', 'l':'Location', 'd':'Date',
+                     'c':'Contact', 'i':'ID', 'a':'Age(>90)',
+                     'o':'Others'}
+    num = 1
+    for (k, v) in summary.items():
+        finpath = os.path.join(anno_path, k + '.ano')
+        # load annotation
+        
+        if os.path.isfile(finpath):
+            with open(finpath, 'rb') as fin:
+                anno = pickle.load(fin)
+            # get FN for a file
+            FN_list = v['false_negative']
+            start_position = 0
+            print(FN_list)
+
+            for i in FN_list:
+                print(num)
+                num+=1
+                iffound = False
+                while True:
+                    old_position = start_position
+                    j = start_position
+                    if j >= len(anno):
+                        break
+                    start_position += 1
+                    if anno[j][0] == i:
+                        try:
+                            FN_category.append(i + ' ' + category_dict[anno[j][1]] + ' ' + k)
+                            iffound = True
+                            if category_dict[anno[j][1]] in FN_dict.keys():
+                                FN_dict[category_dict[anno[j][1]]].append(i)
+                                break
+                            else:
+                                FN_dict[category_dict[anno[j][1]]] = [i]
+                                break
+                        except:
+                            print(i, j[1])
+
+                if not iffound:
+                    FN_category.append(i + ' ' + 'cannot find ' + k)
+                    FN_dict['cannot find'].append(i)
+                    start_position = old_position
+
+
+    return FN_category, FN_dict
+
+
+def main():
+    summary_path = input("where is summary?>")
+    with open(summary_path, 'rb') as fin:
+        summary = pickle.load(fin)
+    anno_path = input("where are annotations?>")
+    FN_category, FN_dict = getcatgory(summary, anno_path)
+
+    with open('FN_category.pkl', 'wb') as fout:
+        pickle.dump(FN_category, fout)
+
+    with open('FN_dict.pkl', 'wb') as fout:
+        pickle.dump(FN_dict, fout)
+
+
+if __name__ == "__main__":
+    main()
--- get_results/ucsf_process.sh
+++ get_results/ucsf_process.sh
@@ -7,9 +7,9 @@
 philter -i /media/DataHD/philter-annotations/pooneh/input_notes/ -o /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/data/ucsf_results/ -r -p 32
 
 # 2. Run evaluation script on de-identified ucsf notes
-python3 /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/philter_eval_pypi.py -p /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/data/ucsf_results/ -a /media/DataHD/philter-annotations/pooneh/pooneh-done/ -o /media/DataHD/r_phi_corpus/kathleen/ucsf_eval/
+python3 /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/philter_eval.py -p /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/data/ucsf_results/ -a /media/DataHD/philter-annotations/pooneh/pooneh-done/ -o /media/DataHD/r_phi_corpus/kathleen/ucsf_eval/
 
-# 3. Run whitelist-only philter on i2b2 dataset
+# 3. Run false negative script
 python3 /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/getFNcategory_updated.py
 # The script will ask you to input the path to the summary dictionary:
 /media/DataHD/r_phi_corpus/kathleen/ucsf_eval/summary_dict.pkl
@@ -17,3 +17,25 @@ python3 /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/getFNcategory_updated.
 /media/DataHD/philter-annotations/pooneh/pooneh-done
 
 # Results json file is outputted to the same directory that the summary file is located in
+# To visualize FN categories, enter these commands into the terminal within the resuls directory:
+python3
+import json
+results = json.loads(open("FN_dict.json").read())
+for subdict in results:
+	print(subdict)
+	print(len(results[subdict]))
+
+
+##### Whitelist only ######
+# 1. From the de-id_stable1 directory, run downloaded and installed philter script on ucsf dataset
+python3 /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/philter_whitelist.py -i /media/DataHD/philter-annotations/pooneh/input_notes/ -o /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/data/ucsf_results_whitelist/ -r -p 32
+
+# 2. Run evaluation script on de-identified ucsf notes
+python3 /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/philter_eval.py -p /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/data/ucsf_results_whitelist/ -a /media/DataHD/philter-annotations/pooneh/pooneh-done/ -o /media/DataHD/r_phi_corpus/kathleen/ucsf_eval_whitelist/
+
+# 3. Run false negative script
+python3 /media/DataHD/r_phi_corpus/kathleen/de-id_stable1/getFNcategory_updated.py
+# The script will ask you to input the path to the summary dictionary:
+/media/DataHD/r_phi_corpus/kathleen/ucsf_eval_whitelist/summary_dict.pkl
+# ...and for the path to the directory containing the annotation files
+/media/DataHD/philter-annotations/pooneh/pooneh-done
