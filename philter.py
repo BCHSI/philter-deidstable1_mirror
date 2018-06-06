b234eb6167f17cb47b65423703cc0b8980cc03d4
Kathleen <kdmuenzen@yahoo.com>
2018-06-06 15:32:03 -0700

    moving endoscope model to unsafe mrn_id regex

--- philter.py
+++ philter.py
@@ -209,7 +209,7 @@ class Philter:
             for m in matches:
                 #if filename == './data/i2b2_notes_updated/373-04.txt':
                 # if self.patterns[pattern_index]["title"] == "YYYY/MM-YYYY/MM":
-                # if 'Wall' in m.group():
+                # if '' in m.group():
                 #     print(self.patterns[pattern_index]["title"])
                 #     print(m.group())
                 
