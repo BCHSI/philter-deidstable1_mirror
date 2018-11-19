81744e94538bb06346ecc7ccedd19d3a101dc828
laxmirad <40216358+laxmirad@users.noreply.github.com>
2018-11-19 13:20:43 -0800

    Removing unwanted spaces

--- subs.py
+++ subs.py
@@ -11,22 +11,22 @@ DEFAULT_SHIFT_VALUE = 32
 class Subs:
     def __init__(self, look_up_table_path = None):
         #load shift table to a dictionary
-        self.shift_table  = self._load_look_up_table(look_up_table_path)	        
+        self.shift_table  = self._load_look_up_table(look_up_table_path)        
      
-    def has_shift_amount(self, note_id):	    
+    def has_shift_amount(self, note_id):
         return note_id in self.shift_table
     
-     def get_shift_amount(self,note_id):	
-        try:	
-            shift_amount = int(self.shift_table***REMOVED***note_id***REMOVED***)	
-            if shift_amount == 0:	
-                print("WARNING: shift amount for note_id: {0} is zero.".format(note_id))	
-        except KeyError as err:	
-            print("Key Error in shift_table {0}".format(err))	
-            shift_amount = None	
-        except ValueError as err:	
-            print("Value Error: date_offset is not an integer for note_id=" + str(note_id) + "{0}".format(err))	
-            shift_amount = None	
+     def get_shift_amount(self,note_id):
+        try:
+            shift_amount = int(self.shift_table***REMOVED***note_id***REMOVED***)
+            if shift_amount == 0:
+                print("WARNING: shift amount for note_id: {0} is zero.".format(note_id))
+        except KeyError as err:
+            print("Key Error in shift_table {0}".format(err))
+            shift_amount = None
+        except ValueError as err:
+            print("Value Error: date_offset is not an integer for note_id=" + str(note_id) + "{0}".format(err))
+            shift_amount = None
         return shift_amount
     
     def shift_date(self, date, shift_amount):
@@ -74,7 +74,7 @@ class Subs:
      def _load_look_up_table(self, look_up_table_path):
         # note_info_path='data/notes_metadata/note_info.csv'
         # re_id_pat_path='data/notes_metadata/re_id_pat.csv'
-        if look_up_table_path is None:	
+        if look_up_table_path is None:
             return {} #defaultdict(lambda:DEFAULT_SHIFT_VALUE)
         look_up_table = pd.read_csv(look_up_table_path, sep='\t', index_col=False, usecols=***REMOVED***'note_key', 'date_offset'***REMOVED***, dtype=str)
         look_up_table = look_up_table***REMOVED***~look_up_table***REMOVED***"date_offset"***REMOVED***.isnull()***REMOVED***
