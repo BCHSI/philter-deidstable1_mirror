c65c5751ec8edfde032f1fef151013a69808e42a
laxmirad <40216358+laxmirad@users.noreply.github.com>
2018-11-19 12:16:20 -0800

    Added the eval function to the existing phitexts code

--- phitexts.py
+++ phitexts.py
@@ -34,7 +34,7 @@ class Phitexts:
         self.xml_map = CoordinateMap()
         self.full_xml_map = {}
         self._read_texts()
-        self.sub = Subs(note_info_path = None, re_id_pat_path = None)
+        self.suber = None
         self.filterer = None
         #create a memory for the list of known PHI types
         self.phi_type_list = ***REMOVED***'Date'***REMOVED***
@@ -56,7 +56,7 @@ class Phitexts:
 
                 self.filenames.append(filepath)
                 encoding = self._detect_encoding(filepath)
-                fhandle = open(filepath, "r", encoding=encoding***REMOVED***'encoding'***REMOVED***)
+                fhandle = open(filepath, "r", encoding=encoding***REMOVED***'encoding'***REMOVED***, errors='surrogateescape')
                 self.texts***REMOVED***filepath***REMOVED*** = fhandle.read()
                 fhandle.close()
 
@@ -225,7 +225,7 @@ class Phitexts:
             if phi_type == "DATE" or phi_type == "Date":
                 for filename, start, end in self.types***REMOVED***phi_type***REMOVED******REMOVED***0***REMOVED***.scan():
                     token = self.texts***REMOVED***filename***REMOVED******REMOVED***start:end***REMOVED***
-                    normalized_token = self.sub.parse_date(token)                  
+                    normalized_token = Subs.parse_date(token)                 
                     self.norms***REMOVED***phi_type***REMOVED*** ***REMOVED***(filename, start)***REMOVED*** = (normalized_token, end)
                 
             else:
@@ -238,7 +238,7 @@ class Phitexts:
 
         # Note: this is currently done in surrogator.shift_dates(), surrogator.parse_and_shift_date(), parse_date_ranges(), replace_other_surrogate()
         
-    def substitute_phi(self):
+    def substitute_phi(self, look_up_table_path = None):
         assert self.norms, "No normalized PHI defined"
         
         if self.subs:
@@ -249,6 +249,10 @@ class Phitexts:
         for phi_type in self.norms.keys():
             if phi_type == "DATE" or phi_type == "Date":
                 for filename, start in self.norms***REMOVED***phi_type***REMOVED***:
+                    note_key_ucsf = os.path.splitext(os.path.basename(filename).strip('0'))***REMOVED***0***REMOVED***	
+                    if not self.subser.has_shift_amount(note_key_ucsf):	
+                        if __debug__: print("WARNING: no date shift found for file: " + filename)	
+                        continue
                     normalized_token = self.norms***REMOVED***phi_type***REMOVED******REMOVED***filename, start***REMOVED******REMOVED***0***REMOVED***
                     end = self.norms***REMOVED***phi_type***REMOVED******REMOVED***filename, start***REMOVED******REMOVED***1***REMOVED***
 
@@ -256,8 +260,7 @@ class Phitexts:
                     if normalized_token is None:
                         # self.eval_table***REMOVED***filename***REMOVED******REMOVED***start***REMOVED***.update({'sub':None})
                         continue
-                    #TODO: why don't we make the lookup by the filename instead of patient_id
-                    substitute_token = self.sub.date_to_string(self.sub.shift_date_pid(normalized_token, filename))
+                    substitute_token = self.subser.date_to_string(self.subser.shift_date_pid(normalized_token, note_key_ucsf))
                     # self.eval_table***REMOVED***filename***REMOVED******REMOVED***start***REMOVED***.update({'sub':substitute_token})
                     self.subs***REMOVED***(filename, start)***REMOVED*** = (substitute_token, end)
             else:
@@ -270,8 +273,9 @@ class Phitexts:
         if not self.coords:
             self.textsout = self.texts
             print("WARNING: No PHI coordinates defined: nothing to transform!")
-        else:
-            assert self.subs, "No surrogated PHI defined"
+        #Subs may be empty in the case where we do not have any date shifts to perform.
+        #else:
+            #assert self.subs, "No surrogated PHI defined"
                 
         if self.textsout:
             return
@@ -309,7 +313,7 @@ class Phitexts:
             self.textsout***REMOVED***filename***REMOVED*** =  "".join(contents)
 
     def _transform_text(self, txt, infilename,
-                                include_map, exclude_map, subs):       
+                        include_map, exclude_map, subs):       
         last_marker = 0
         current_chunk = ***REMOVED******REMOVED***
         punctuation_matcher = re.compile(r"***REMOVED***^a-zA-Z0-9****REMOVED***")
@@ -351,7 +355,7 @@ class Phitexts:
             fbase, fext = os.path.splitext(filename)
             fbase = fbase.split('/')***REMOVED***-1***REMOVED***
             filepath = outputdir + fbase + "_subs.txt"
-            with open(filepath, "w", encoding='utf-8') as fhandle:
+            with open(filepath, "w", encoding='utf-8', errors='surrogateescape') as fhandle:
                 fhandle.write(self.textsout***REMOVED***filename***REMOVED***)
     
     def print_log(self, output_dir):
@@ -386,8 +390,12 @@ class Phitexts:
             raw = self.texts***REMOVED***filename***REMOVED******REMOVED***start:end***REMOVED***
             normalized_date = self.norms***REMOVED***phi_type***REMOVED******REMOVED***(filename,start)***REMOVED******REMOVED***0***REMOVED***
             if normalized_date is not None:
-                normalized_token = self.sub.date_to_string(normalized_date)
-                sub = self.subs***REMOVED***(filename,start)***REMOVED******REMOVED***0***REMOVED***
+                normalized_token = self.subser.date_to_string(normalized_date)
+                note_key_ucsf = os.path.splitext(os.path.basename(filename).strip('0'))***REMOVED***0***REMOVED***
+                if not self.subser.has_shift_amount(note_key_ucsf):	
+                     sub = None	
+                else:	
+                     sub = self.subs***REMOVED***(filename,start)***REMOVED******REMOVED***0***REMOVED***
                 num_parsed += 1
                 if filename not in eval_table:
                     eval_table***REMOVED***filename***REMOVED*** = ***REMOVED******REMOVED***
@@ -438,6 +446,175 @@ class Phitexts:
         with open(phi_marked_file, 'w') as f:
             json.dump(phi_table, f)
     
+    def eval(self, anno_dir, in_dir, output_dir):
+        # preserve these two puncs so that dates are complete
+        s = string.punctuation.replace('/','')
+        s = s.replace('-', '')
+        translator = str.maketrans('', '', s)
+
+        gold_phi = {}
+        eval_dir = os.path.join(output_dir, 'eval/')
+        summary_file = os.path.join(eval_dir, 'summary.json')
+        json_summary_by_file = os.path.join(eval_dir, 'summary_by_file.json')
+        json_summary_by_category = os.path.join(eval_dir, 'summary_by_category.json')
+
+        if os.path.isdir(eval_dir):
+            pass
+        else:
+            os.makedirs(eval_dir)
+
+        for root, dirs, files in os.walk(anno_dir):
+            for filename in files:
+                if not filename.endswith("xml"):
+                    continue                
+                filepath = os.path.join(root, filename)
+                # change here: what will the input format be?
+                file_id = in_dir + filename.split('.')***REMOVED***0***REMOVED*** + '.txt'
+                tree = ET.parse(filepath)
+                root = tree.getroot()
+                xmlstr = ET.tostring(root, encoding='utf8', method='xml')
+                xml_dict = xmltodict.parse(xmlstr)***REMOVED***'deIdi2b2'***REMOVED***
+                text = xml_dict***REMOVED***"TEXT"***REMOVED***
+                tags_dict = xml_dict***REMOVED***"TAGS"***REMOVED***
+                if file_id  not in gold_phi:
+                   gold_phi***REMOVED***file_id***REMOVED*** = {}
+                # the existence of puncs in text makes the end index inaccurate - only use start index as the key
+                for key, value in tags_dict.items():
+                    if isinstance(value, list):
+                        for final_value in value:
+                            start = int(final_value***REMOVED***"@start"***REMOVED***)
+                            end = int(final_value***REMOVED***"@end"***REMOVED***)
+                            text = final_value***REMOVED***"@text"***REMOVED***.translate(translator)
+                            phi_type = final_value***REMOVED***"@TYPE"***REMOVED***
+                            gold_phi***REMOVED***file_id***REMOVED***.update({start:***REMOVED***end,phi_type,text***REMOVED***})
+
+                    else:
+                        final_value = value
+                        start = int(final_value***REMOVED***"@start"***REMOVED***)
+                        end = int(final_value***REMOVED***"@end"***REMOVED***)
+                        text = final_value***REMOVED***"@text"***REMOVED***.translate(translator)
+                        phi_type = final_value***REMOVED***"@TYPE"***REMOVED***
+                        gold_phi***REMOVED***file_id***REMOVED***.update({start:***REMOVED***end, phi_type, text***REMOVED***})
+       
+        # converting self.types to an easier accessible data structure
+        eval_table = {}
+        phi_table = {}
+        non_phi = {}
+        for phi_type in self.types:
+            for filename, start, end in self.types***REMOVED***phi_type***REMOVED******REMOVED***0***REMOVED***.scan():
+                word = self.texts***REMOVED***filename***REMOVED******REMOVED***start:end***REMOVED***.translate(translator)
+                if filename not in phi_table:
+                    phi_table***REMOVED***filename***REMOVED*** = {}
+                phi_table***REMOVED***filename***REMOVED***.update({start:***REMOVED***end, phi_type, word***REMOVED***})
+        for filename in gold_phi:
+            if filename not in eval_table:
+                eval_table***REMOVED***filename***REMOVED*** = {'fp':{},'tp':{},'fn':{},'tn':{}}
+            # each ele contains an annotated phi
+            # token_set = self._get_clean(self.texts***REMOVED***filename***REMOVED***)
+            text = self.texts***REMOVED***filename***REMOVED***
+            for start in gold_phi***REMOVED***filename***REMOVED***:
+                gold_start = start
+                gold_end = gold_phi***REMOVED***filename***REMOVED******REMOVED***start***REMOVED******REMOVED***0***REMOVED***
+                gold_type = gold_phi***REMOVED***filename***REMOVED******REMOVED***start***REMOVED******REMOVED***1***REMOVED***
+                gold_word = gold_phi***REMOVED***filename***REMOVED******REMOVED***start***REMOVED******REMOVED***2***REMOVED***
+                # remove phi from text to form the non_phi_set
+                text = text.replace(gold_word, '')
+                if filename in phi_table:
+                    # is phi and is caught -> TP
+                    if gold_start in phi_table***REMOVED***filename***REMOVED***:
+                        word = phi_table***REMOVED***filename***REMOVED******REMOVED***gold_start***REMOVED******REMOVED***2***REMOVED***
+                        if word == gold_word:
+                            if gold_type not in eval_table***REMOVED***filename***REMOVED******REMOVED***'tp'***REMOVED***:
+                                eval_table***REMOVED***filename***REMOVED******REMOVED***'tp'***REMOVED******REMOVED***gold_type***REMOVED*** = ***REMOVED******REMOVED***
+                            eval_table***REMOVED***filename***REMOVED******REMOVED***'tp'***REMOVED******REMOVED***gold_type***REMOVED***.append(word)
+                    # is phi but not caught -> FN
+                    else:
+                        # word = phi_table***REMOVED***filename***REMOVED******REMOVED***gold_start***REMOVED******REMOVED***2***REMOVED***
+                        if gold_type not in eval_table***REMOVED***filename***REMOVED******REMOVED***'fn'***REMOVED***:
+                            eval_table***REMOVED***filename***REMOVED******REMOVED***'fn'***REMOVED******REMOVED***gold_type***REMOVED*** = ***REMOVED******REMOVED***
+                        eval_table***REMOVED***filename***REMOVED******REMOVED***'fn'***REMOVED******REMOVED***gold_type***REMOVED***.append(gold_word)
+                else:
+                    print (filename + ' not processed by philter or check filename!')
+                    continue
+            non_phi***REMOVED***filename***REMOVED*** = self._get_clean(text)
+
+        for filename in phi_table:
+            if filename in gold_phi:
+                if filename not in eval_table:
+                    eval_table***REMOVED***filename***REMOVED*** = {'fp':{},'tp':{},'fn':{},'tn':***REMOVED******REMOVED***}
+                for start in phi_table***REMOVED***filename***REMOVED***:
+                    end = phi_table***REMOVED***filename***REMOVED******REMOVED***start***REMOVED******REMOVED***0***REMOVED***
+                    phi_type = phi_table***REMOVED***filename***REMOVED******REMOVED***start***REMOVED******REMOVED***1***REMOVED***
+                    word = phi_table***REMOVED***filename***REMOVED******REMOVED***start***REMOVED******REMOVED***2***REMOVED***
+                    # print (word)
+                    # word caught but is not phi -> FP
+                    if start not in gold_phi:
+                        if phi_type not in eval_table***REMOVED***filename***REMOVED******REMOVED***'fp'***REMOVED***:
+                            eval_table***REMOVED***filename***REMOVED******REMOVED***'fp'***REMOVED******REMOVED***phi_type***REMOVED*** = ***REMOVED******REMOVED***
+                            eval_table***REMOVED***filename***REMOVED******REMOVED***'fp'***REMOVED******REMOVED***phi_type***REMOVED***.append(word)
+                            if word in non_phi***REMOVED***filename***REMOVED***:
+                                non_phi***REMOVED***filename***REMOVED***.remove(word)
+            else:
+                print (filename + ' not found!')
+        # the rest is all TN
+        for filename in non_phi:
+            if filename not in eval_table:
+                eval_table***REMOVED***filename***REMOVED*** = {'fp':{},'tp':{},'fn':{},'tn':{}}
+            eval_table***REMOVED***filename***REMOVED******REMOVED***'tn'***REMOVED*** = non_phi***REMOVED***filename***REMOVED***
+
+        summary_by_category = {}
+        summary_by_file = {}
+
+        total_tp = 0
+        total_fn = 0
+        total_tn = 0
+        total_fp = 0
+        for filename in eval_table:
+            # file-level counters
+            tp = 0
+            fn = 0
+            tn = 0
+            fp = 0
+            if filename not in summary_by_file:
+                summary_by_file***REMOVED***filename***REMOVED*** = {}
+            for phi_type in eval_table***REMOVED***filename***REMOVED******REMOVED***'tp'***REMOVED***:
+                tp += len(eval_table***REMOVED***filename***REMOVED******REMOVED***'tp'***REMOVED******REMOVED***phi_type***REMOVED***)
+                total_tp += tp
+                if phi_type not in summary_by_category:
+                    summary_by_category***REMOVED***phi_type***REMOVED*** = {}
+                if 'tp' not in summary_by_category***REMOVED***phi_type***REMOVED***:
+                    summary_by_category***REMOVED***phi_type***REMOVED******REMOVED***'tp'***REMOVED*** = ***REMOVED******REMOVED***
+                summary_by_category***REMOVED***phi_type***REMOVED******REMOVED***'tp'***REMOVED***.append(eval_table***REMOVED***filename***REMOVED******REMOVED***'tp'***REMOVED******REMOVED***phi_type***REMOVED***)
+            for phi_type in eval_table***REMOVED***filename***REMOVED******REMOVED***'fp'***REMOVED***:
+                fp += len(eval_table***REMOVED***filename***REMOVED******REMOVED***'fp'***REMOVED******REMOVED***phi_type***REMOVED***)
+                total_fp += fp
+                if phi_type not in summary_by_category:
+                    summary_by_category***REMOVED***phi_type***REMOVED*** = {}
+                if 'fp' not in summary_by_category***REMOVED***phi_type***REMOVED***:
+                    summary_by_category***REMOVED***phi_type***REMOVED******REMOVED***'fp'***REMOVED*** = ***REMOVED******REMOVED***
+                summary_by_category***REMOVED***phi_type***REMOVED******REMOVED***'fp'***REMOVED***.append(eval_table***REMOVED***filename***REMOVED******REMOVED***'fp'***REMOVED******REMOVED***phi_type***REMOVED***)
+            for phi_type in eval_table***REMOVED***filename***REMOVED******REMOVED***'fn'***REMOVED***:
+                tp += len(eval_table***REMOVED***filename***REMOVED******REMOVED***'fn'***REMOVED******REMOVED***phi_type***REMOVED***)
+                total_tp += tp
+                if phi_type not in summary_by_category:
+                    summary_by_category***REMOVED***phi_type***REMOVED*** = {}
+                if 'fn' not in summary_by_category***REMOVED***phi_type***REMOVED***:
+                    summary_by_category***REMOVED***phi_type***REMOVED******REMOVED***'fn'***REMOVED*** = ***REMOVED******REMOVED***
+                summary_by_category***REMOVED***phi_type***REMOVED******REMOVED***'fn'***REMOVED***.append(eval_table***REMOVED***filename***REMOVED******REMOVED***'fn'***REMOVED******REMOVED***phi_type***REMOVED***)
+            tn = len(eval_table***REMOVED***filename***REMOVED******REMOVED***'tn'***REMOVED***)
+            total_tn += tn
+            precision = tp / (tp + fp)
+            recall = tp / (tp + fn)
+            summary_by_file***REMOVED***filename***REMOVED***.update({'tp':tp, 'fp': fp, 'fn':fn, 'tn':tn, 'recall':recall,'precision':precision})
+            # summary_by_category***REMOVED***filename***REMOVED***.update({'tp':tp, 'fp': fp, 'fn':fn, 'tn':tn, 'recall':recall,'precision':precision})
+        
+       
+        total_precision = total_tp / (total_tp + total_fp)
+        total_recall = total_tp / (total_tp + total_fn)
+        total_summary = {'tp':total_tp, 'tn':total_tn, 'fp':total_fp, 'fn':total_fn, 'precision':total_precision, 'recall':total_recall}
+        json.dump(total_summary, open(summary_file, "w"), indent=4)
+        json.dump(summary_by_file, open(json_summary_by_file, "w"), indent=4)
+        json.dump(summary_by_category, open(json_summary_by_category, "w"), indent=4)
 
 
 
