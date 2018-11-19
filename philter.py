fd482af048d555fe5d7d4577259c534a07a1ceab
laxmirad <40216358+laxmirad@users.noreply.github.com>
2018-11-19 11:59:56 -0800

    Merged one additional change to philter.py which was missed in the previous manual merge

--- philter.py
+++ philter.py
@@ -158,6 +158,9 @@ class Philter:
                 self.pos_tags = {}
                 self.pos_tags***REMOVED***filename***REMOVED*** = nltk.pos_tag(cleaned)
             return self.pos_tags***REMOVED***filename***REMOVED***
+        
+             #self.pos_tags***REMOVED***filename***REMOVED*** = nltk.pos_tag(cleaned)	
+        return self.pos_tags***REMOVED***filename***REMOVED***
     #def get_pos_original(self, filename, cleaned):
     #    if filename not in self.pos_tags:
     #        self.pos_tags = {}
