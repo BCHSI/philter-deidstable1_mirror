7286742d9884fa64931af2615e23bc10c8a13718
RedChrists <gundolf.schenk@ucsf.edu>
2018-12-07 13:22:46 -0800

    making deidloop and deidmaster work together

--- deidmaster.py
+++ deidmaster.py
@@ -6,20 +6,20 @@ from subprocess import call
 username = "schenkg"
 servers = {'MCWLDEIDLAP701.ucsfmedicalcenter.org':24,
            'qcdeidlap702.ucsfmedicalcenter.org':14,
-           'qcdeidlap703.ucsfmedicalcenter.org':14}#,
-           #'qcdeidlap705.ucsfmedicalcenter.org':14}
+           'qcdeidlap703.ucsfmedicalcenter.org':14,
+           'qcdeidlap705.ucsfmedicalcenter.org':14}
 
-srcBase = "/data/notes/shredded_notes/000/000/000/"
-dstBase = "/data/deid_notes_20180328/000/000/000/"
-mtaBase = "/data/notes/meta_data_20180328/000/000/000/"
-wrkDir = "/data/schenkg/" #dstBase
+srcBase = "/data/notes/shredded_notes/"
+dstBase = "/data/schenkg/deid_notes_20180328_ttt/"
+mtaBase = "/data/notes/meta_data_20180328/"
+wrkDir = "/data/schenkg/pipeline/" #dstBase
 
 # list all subdirs in srcBase
 srcFolders = []
 print("walking through {0} (this can take a couple of minutes)".format(srcBase))
 for root, dirs, files in os.walk(srcBase):
     if not dirs:
-        srcFolders.append(root + '/')
+        srcFolders.append(os.path.join(root, '')) # adds trailing slash
         #print("found dir {0}".format(root))
     
 # shuffle subdirs list to balance out across servers
@@ -29,20 +29,19 @@ random.shuffle(srcFolders)
 
 srcMetafiles = []
 dstFolders = []
-print("creating metafiles list in {0} and output subdirs list in {1}".format(mtaBase, dstBase))
+print("creating metafiles list in " + mtaBase
+      + " and output subdirs list in " +  dstBase)
 for srcFolder in srcFolders:
     srcMetafiles.append(os.path.join(mtaBase, os.path.relpath(srcFolder,
                                                               srcBase),
                                      "meta_data.txt"))
-    dstFolders.append(os.path.join(dstBase,
-                                   os.path.relpath(srcFolder, srcBase)) + '/')
+    dstFolders.append(os.path.join(dstBase, os.path.relpath(srcFolder, srcBase),
+                                   ''))
 
 # split into chunks based on number_of_servers & number_of_threads_per_each_server
 total_threads = sum(servers.values())
 chunk_len = []
-chunks_srcFolders_fname = {}
-chunks_srcMetafiles_fname = {}
-chunks_dstFolders_fname = {}
+chunks_fname = {}
 for url, nthreads in servers.items():
     subdirs_fraction = nthreads / total_threads
     chunk_len.append(ceil(subdirs_fraction * len(srcFolders)))
@@ -54,24 +53,28 @@ for url, nthreads in servers.items():
     chunk_srcFolders = srcFolders[start:end]
     chunk_srcMetafiles = srcMetafiles[start:end]
     chunk_dstFolders = dstFolders[start:end]
-    chunks_srcFolders_fname[url] = "srcfolders_" + url.split('.')[0] + ".txt"
-    chunks_srcMetafiles_fname[url] = "srcmetafiles_" + url.split('.')[0] + ".txt"
-    chunks_dstFolders_fname[url] = "dstfolders_" + url.split('.')[0] + ".txt"
-    with open(chunks_srcFolders_fname[url], 'w') as cf:
-        cf.write('\n'.join(chunk_srcFolders))
-        cf.write('\n')
-    with open(chunks_srcMetafiles_fname[url], 'w') as cf:
-        cf.write('\n'.join(chunk_srcMetafiles))
-        cf.write('\n')
-    with open(chunks_dstFolders_fname[url], 'w') as cf:
-        cf.write('\n'.join(chunk_dstFolders))
-        cf.write('\n')
-    # os.system("scp {0} {1} {2} {3}@{4}:{5}".format(chunks_srcFolders_fname[url], chunks_srcMetafiles_fname[url], chunks_dstFolders_fname[url],
-    #                                                username, url, wrkDir))
+    chunks_fname[url] = "chunk_" + url.split('.')[0] + ".csv"
+    with open(chunks_fname[url], 'w') as cf:
+        for idir, mfile, odir in zip(chunk_srcFolders, chunk_srcMetafiles,
+                                     chunk_dstFolders):
+            cf.write("{0} {1} {2}\n".format(idir, mfile, odir))
+    print("copying imo chunk file to server " + url)
+    os.system("scp {0} {1}@{2}:{3}".format(chunks_fname[url], username, url,
+                                           wrkDir))
     
 # ssh into each server and pass respective subdirs chunk to deidloop
 for url, nthreads in servers.items():
-    commandline = "ssh {0}@{1} ".format(username, url) + "echo \"/usr/bin/time nice python3 {2}deidloop.py {1} {2}{3} {2}{4} {2}{5} > {2}stdouterr_{0}.txt 2>&1 &\"".format(url.split('.')[0], nthreads, wrkDir, chunks_srcFolders_fname[url], chunks_srcMetafiles_fname[url], chunks_dstFolders_fname[url])
+    commandline = ("ssh {0}@{1}".format(username, url)
+                   #+ " hostname && echo \""
+                   + " nohup /usr/bin/time nice python3 "
+                   + os.path.join(wrkDir, "deidloop.py")
+                   + " -t {0} ".format(nthreads)
+                   + " --imofile " + os.path.join(wrkDir, chunks_fname[url])
+                   + " --philter " + wrkDir
+                   + " --superlog " + dstBase
+                   + " > " + os.path.join(wrkDir, "stdouterr_"
+                                          + url.split('.')[0] + ".txt")
+                   + " 2>&1 &")
     print(commandline)
     call(commandline.split())
 
