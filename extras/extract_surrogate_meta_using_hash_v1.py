1646eb0be38639b3a25f36f93fbb267d793460d3
Lakshmi Radhakrishnan <laxmi.rad@gmail.com>
2019-01-25 15:15:48 -0800

    Adding supporting scripts to extras folder

---
+++ extras/extract_surrogate_meta_using_hash_v1.py
@@ -0,0 +1,43 @@
+#############################
+#Program Usage:
+## Python3 extract_using_hash_v1.py <file with the list of directory to traverse> <META 58M file>
+############################
+import sys
+
+dirpath = sys.argv***REMOVED***1***REMOVED***
+mfpath = sys.argv***REMOVED***2***REMOVED***
+dirfile = open(dirpath)
+
+# Generate the Hash table for the Meta file
+
+meta = {}
+mfile = open(mfpath)
+for line in mfile:
+    line = line.rstrip('\n')
+    line  = line.replace('.0','')
+    key,value = line.split('\t',1)
+    meta***REMOVED***key***REMOVED*** = value
+print("loaded into hash")
+#Walk through each of the directory in the list and create batch meta files for each of the batches
+for dirline in dirfile:
+    dirline = dirline.replace('\n','')
+    kpath = dirline+'/batch_input.in'
+    dfile = open(dirline+"/meta_data.txt", "w+")     
+    #Generate keeper set
+    keepers = set()
+    kdict = dict()
+    kfile = open(kpath)
+    #head = kfile.readline()
+    for line in kfile:
+        line = line.replace('\n','')
+        line = line.replace('.txt','')
+        line = line.lstrip('0')
+        keepers.add(line)
+    kfile.close()
+    print("Creating meta file for"+dirline)
+    dfile.write("note_key"+"\t"+"date_offset"+"\t"+"patient_ID"+"\t"+"deid_note_key"+"\n")
+    for k in keepers:
+        if k in meta:
+           dfile.write(k+"\t"+meta***REMOVED***k***REMOVED***+"\n")
+dirfile.close()      
+   
