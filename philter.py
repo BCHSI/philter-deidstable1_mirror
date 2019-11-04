851703c8cbfb2c45f3191a17ec336cecea19c034
Kathleen <kdmuenzen@yahoo.com>
2019-11-04 15:07:42 -0800

    removing print statement

--- philter.py
+++ philter.py
@@ -677,9 +677,7 @@ class Philter:
                    note_key = file_note_key
                    # Issue: dictionary of dynamic blacklist words also not in list format (only showing single note key per word)
                    for key in self.patterns***REMOVED***pattern_index***REMOVED******REMOVED***"data"***REMOVED***:
-                       if note_key in self.patterns***REMOVED***pattern_index***REMOVED******REMOVED***"data"***REMOVED******REMOVED***key***REMOVED***: 
-                          print(self.patterns***REMOVED***pattern_index***REMOVED******REMOVED***"data"***REMOVED******REMOVED***key***REMOVED***) 
-                          print('key found')       
+                       if note_key in self.patterns***REMOVED***pattern_index***REMOVED******REMOVED***"data"***REMOVED******REMOVED***key***REMOVED***:      
                           key_clean = re.sub(r"***REMOVED***^a-zA-Z0-9***REMOVED***+", "", str(key).lower().strip()) 
                           map_set***REMOVED***key_clean***REMOVED*** = self.patterns***REMOVED***pattern_index***REMOVED******REMOVED***"data"***REMOVED******REMOVED***key***REMOVED***
                           #print(filename)
