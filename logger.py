40c0dbfebb0508241bd9fff763fc7c9a1f1b6a58
Kathleen <kdmuenzen@yahoo.com>
2019-11-21 10:14:21 -0800

    fixing indentation errors

--- logger.py
+++ logger.py
@@ -21,7 +21,7 @@ def get_args():
                     + " super log in a subfolder log of the set folder"
                     + " combining logs of each output directory",
                     type=str)
-
+    
     return ap.parse_args()
 
 
@@ -124,20 +124,20 @@ def get_super_log(all_logs, super_log_dir):
 
 
 def create_log_files_list(imofile):
-        all_logs = []
-        with open(args.imofile, 'r') as imo:
-            for line in imo:
-                parts = line.split()
-                if len(parts) > 3:
-                   idir, mfile, odir, kpfile = line.split()
-                else:
-                   idir, mfile, odir = line.split()
+    all_logs = []
+    with open(imofile, 'r') as imo:
+        for line in imo:
+            parts = line.split()
+            if len(parts) > 3:
+                idir, mfile, odir, kpfile = line.split()
+            else:
+                idir, mfile, odir = line.split()
 
-                all_logs.append(os.path.join(odir, "log",
+            all_logs.append(os.path.join(odir, "log",
                                              "detailed_batch_summary.csv"))
-                all_logs.append(os.path.join(odir, "log",
+            all_logs.append(os.path.join(odir, "log",
                                              "dynamic_blacklist_summary.csv"))
-            
+    
     return all_logs
         
 def main():
