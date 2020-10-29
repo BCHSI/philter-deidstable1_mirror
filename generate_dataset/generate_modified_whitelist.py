6baa4221a3243e936dade0618528b0374c58a558
Kathleen <kdmuenzen@yahoo.com>
2020-10-29 13:46:31 -0700

    adding modified ucsf whitelist and corresponding theta config

--- generate_dataset/generate_modified_whitelist.py
+++ generate_dataset/generate_modified_whitelist.py
@@ -1,18 +1,35 @@
 import json
 import pandas as pd
 import argparse
+import os
 
-def generate_whitelist(whitelist, additions, outfile):
+
+def generate_whitelist(whitelist, additions, removals, outfile):
 
     whitelist = json.loads(open(whitelist).read())
 
-    additions = open(additions).readlines()
+    # Add tokens
+    if additions != None:
+        additions = open(additions).readlines()
 
-    new_words = list(additions)
+        new_words = list(additions)
 
-    for word in new_words:
-        word = word.strip()
-        whitelist[word] = 1
+        for word in new_words:
+            #print(word)
+            word = word.strip()
+            whitelist[word] = 1
+
+    # Remove tokens
+    if removals != None:
+        removals = open(removals).readlines()
+
+        words_to_remove = list(removals)
+
+        for word in words_to_remove:
+            print(word)
+            whitelist.pop(word, None)
+
+
 
     with open(outfile,'w') as file:
         json.dump(whitelist, file)
@@ -27,7 +44,10 @@ def main():
                     help="Path to the base whitelist",
                     type=str)
     ap.add_argument("-a", "--additions",
-                    help="Path to the newline txt file that contains the new words to add",
+                    help="Path to the txt file that contains the new words to add",
+                    type=str)
+    ap.add_argument("-r", "--removals",
+                    help="Path to the txt file that contains the words to remove",
                     type=str)
     ap.add_argument("-o", "--outfile",
                     help="Name of the output whitelist",
@@ -38,9 +58,10 @@ def main():
 
     base_whitelist = args.whitelist
     additions = args.additions
+    removals = args.removals
     outfile = args.outfile
 
-    generate_whitelist(base_whitelist, additions, outfile)
+    generate_whitelist(base_whitelist, additions, removals, outfile)
 
 
 
